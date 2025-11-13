bb8d89b6c71205ead5d33f76ff54af800a82e3a5
skycherepanya <skycherepanya@gmail.com>
2025-11-13 13:38:05 +0100

    Add universal JSON storage module (storage.py) and gitignore

--- bot/storage.py
+++ bot/storage.py
@@ -1,1 +1,39 @@
 # робота з JSON файлом (читання/запис)
+import json
+from pathlib import Path
+import os
+
+DATA_DIR = Path(__file__).parent.parent / 'data'
+
+if not os.path.exists(DATA_DIR):
+    os.makedirs(DATA_DIR)
+
+def save_to_json(data, filename): 
+
+    if not isinstance(data, (dict, list)):
+        raise TypeError("Дані для JSON мають бути словником або списком.")
+
+    file_path = DATA_DIR / filename
+    # try:
+    with open(file_path, 'w', encoding='utf-8') as f:
+        json.dump(data, f, indent=4)
+
+    # except
+
+
+def load_from_json(filename):
+
+    file_path = DATA_DIR / filename  
+
+    try:
+        with open(file_path, 'r', encoding='utf-8') as f:
+            content = f.read()
+            if not content:
+                return {}
+            return json.loads(content)
+            
+    except FileNotFoundError:
+        return {}
+    except json.JSONDecodeError:
+        print(f"Попередження: Файл {file_path} пошкоджено. Завантажено порожні дані.")
+        return {}
